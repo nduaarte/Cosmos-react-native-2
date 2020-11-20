2d2b405ba09ec6b998c96463652561dd6bc2fad2
Nycollas <nycollas.duaarte@hotmail.com>
2020-11-20 02:04:43 -0300

    update

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
     <View >
-
+      <Text>Hellooooo</Text>
     </View>
   );
 }
---
+++ src/layouts/main/Clock/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+const Clock: React.FC = () => {
+  return(
+    <Container>
+      
+    </Container>
+  );
+}
+
+export default Clock;
