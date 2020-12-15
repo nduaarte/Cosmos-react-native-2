0ed10d269740a480f3d9b8b3c48bab648b3ec44d
Nycollas Duarte <nycollas.duaarte@hotmail.com>
2020-12-15 20:56:25 -0300

    last-update-?

--- src/layouts/SimpleModern/StartButton/index.tsx
+++ src/layouts/SimpleModern/StartButton/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Alert } from 'react-native';
 import { useDispatch, useSelector } from "react-redux";
 
 import { RootState } from '../../../Redux/reducers/TimeReducer';
@@ -6,9 +7,14 @@ import { Container, Button } from './styles';
 
 const StartButton: React.FC = () => {
   const timerOn = useSelector((state: RootState) => state.TimeReducer.timerOn);
+  const time = useSelector((state: RootState) => state.TimeReducer.time);
   const dispatch = useDispatch();
 
   function toggleTimerDispatch() {
+    if(time === 0) {
+      return Alert.alert('Por favor, selecione algum tempo para poder iniciar!');
+    }
+
     dispatch({ type: 'UPDATE_TIMERON', value: !timerOn });
   }
 
--- src/layouts/SimpleModern/index.tsx
+++ src/layouts/SimpleModern/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
-import { useSelector } from 'react-redux';
+import { Alert, View } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { RootState } from '../../Redux/reducers/TimeReducer';
 import Clock from './Clock';
@@ -11,20 +11,32 @@ const SimpleModern: React.FC = () => {
   const timerOn = useSelector((state: RootState) => state.TimeReducer.timerOn);
   const time = useSelector((state: RootState) => state.TimeReducer.time);
   const [timing, setTiming] = useState(time);
+  const dispatch = useDispatch();
 
+  // Contador.
   useEffect(() => {
     let interval: any = null;
+
     if(timerOn) {
       interval = setInterval(() => {
         setTiming((timing: number)=> timing - 1000);
-        
       }, 1000);
-    }     
+    }
 
     return () => clearInterval(interval);
   }, [timerOn]);
 
-  // Alternar entre POMODORO ou BREAK. 
+  // Parar contador.
+  useEffect(() => {
+    if(timing === 0) {
+      Alert.alert('Tempo acabou!!');
+      dispatch({ type: 'UPDATE_TIMERON', value: false });
+      dispatch({ type: 'UPDATE_TIME', value: 0 });
+    }
+  }, [timing]);
+
+
+  // Verifica para alternar entre POMODORO ou BREAK. 
   useEffect(() => {
     setTiming(time);
   }, [time]);
